97557fcbe4bf5a2e2b7acb200f588340b46de2c2
Lisa Yee <lisayee@live.com>
2014-09-04 10:46:11 -0400

    after running

--- src/myapplication/Main.java
+++ src/myapplication/Main.java
@@ -11,7 +11,7 @@ public class Main {
         new Thread1(myLock1, myLock2).start();
         new Thread2(myLock1, myLock2).start();
         
-        Thread.sleep(500000);
+        Thread.sleep(100);
         System.out.println("Main thread finished");
     }
     
